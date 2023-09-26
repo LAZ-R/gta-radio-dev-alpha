c42e358f7837bbc31449e7b3f885793a10f23765
LAZ-R <crazydrawer62@gmail.com>
2023-09-27 00:00:05 +0200

    v 0.0.2 : ajout like et fix css et musique menu

--- js/data/app.data.js
+++ js/data/app.data.js
@@ -1,3 +1,3 @@
 export const APP_NAME = "GTA Radios"
-export const APP_VERSION_NUMBER = '0.0.1';
-// v 0.0.1 : init depot
+export const APP_VERSION_NUMBER = '0.0.2';
+// v 0.0.2 : ajout like et fix css et musique menu
